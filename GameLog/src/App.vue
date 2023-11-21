69d330bd6179e8ca5c345c8efff07dc80f932a89
nando008 <josefdorobles0801@gmail.com>
2023-11-21 02:27:48 -0500

    actualizacion

--- GameLog/src/App.vue
+++ GameLog/src/App.vue
@@ -1,4 +1,21 @@
 <script setup>
+import { ref } from 'vue';
+
+
+let one = ref(true)
+let two = ref(false)
+
+function screenOne() {
+  one.value = false
+  two.value = true
+}
+
+function screenTwo(){
+  one.value = true
+  two.value = false
+}
+
+
 
 </script>
 
@@ -6,6 +23,7 @@
   <body>
     <div class="container">
       
+      <div v-show="one">
       <div class="ContainText">
         <h1 class="text">HOLA AMIGUITO!!</h1>
         <h1 class="text">Antes de comenzar esta emocionante aventura debes iniciar sesión</h1>
@@ -21,12 +39,31 @@
         <label>Contraseña</label>  
       </div>
 
-      <div class="button">
-        <a href="">INICIAR</a>
+      <div class="button" @click="screenOne">
+        <button>INICIAR</button>
       </div>
 
       <div class="button">
-        <a href="">REGISTRATE</a>
+        <button href="">REGISTRATE</button>
+      </div>
+      </div>
+
+      <div class="containerTwo" v-show="two">
+        <div class="control">
+          <img src="./Img/control.jpeg" alt="" height="290px" width="500px">
+        </div>
+        
+        <h1 class="textExplicacion">1. Sensor tactil que mueve la nave horizontalmente hacia la izquierda</h1>
+
+        <h1 class="textExplicacion">2. Sensor tactil que mueve la nave horizontalmente hacia la derecha</h1>
+
+        <h1 class="textExplicacion">3. Sensor tactil de disparos con largo alcanze para derribar naves de la primera fila</h1>
+
+        <h1 class="textExplicacion">4. Sensor tactil de disparos con corto alcnaze para derribar naves de la segunda fila</h1>
+
+        <div class="button" @click="screenTwo" >
+          <button href="">REGRESAR</button>
+        </div>
       </div>
 
     </div>
@@ -108,15 +145,57 @@ div > .text{
 .button{
   display: flex;
   justify-content: center;
-  margin-top: 15px;
   font-size: 20px;
   border-radius: 10px;
   background-color: rgba(255, 255, 255, 0.515);
-  margin: 10px 40% 5px 40%;
+  margin: 14px 40% 5px 40%;
   padding: 5px 0px 5px 0px;
   color: #000000;
   font-family: Concert One;
+  
 }
 
+button {
+  text-decoration: none;
+  font-family: Concert One;
+  background-color: transparent;
+  border: none;
+  border-bottom: 2px solid;
+  font-size: 20px;
+}
+
+.button:hover {
+  background-color: rgb(112, 2, 2);
+  transition: 1s;
+  color: #fff;
+}
+
+button:hover {
+  color: #fff;
+  transition: 0.30s;
+}
+
+.container > .containerTwo > .control{
+  height: auto;
+  width: 850px;
+  display: flex;
+  justify-content: center;
+  margin: 18px 0 20px 0;
+}
+
+.textExplicacion {
+  display: flex;
+  flex-direction: column;
+  background-color: rgba(255, 255, 255, 0.515);
+  margin: 8px 40px 0px 40px;
+  border-radius: 10px;
+  padding: 5px 0px 5px 10px;
+  color: rgba(0, 0, 0, 0.853);
+  font-family: Concert One;
+  font-size: 20px;
+}
+
+
+ 
 
 </style>
